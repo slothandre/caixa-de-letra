93d4e84b292797147e720a31d980d4799b8c84fa
André Marques <andre.m.d.abreu@gmail.com>
2024-03-04 09:44:42 -0300

    Update BuscarFilmes.js

--- src/screens/BuscarFilmes.js
+++ src/screens/BuscarFilmes.js
@@ -1,67 +1,70 @@
-import {
-  Alert,
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  Vibration,
-  View,
-} from "react-native";
-import React from "react";
-import SafeContainer from "../components/SafeContainer";
-import { Ionicons } from "@expo/vector-icons";
-
 export default function BuscarFilmes() {
-  const [text, onChangeText] = React.useState("");
+  const [filme, setFilme] = useState("");
 
-  const pesquisar = () => {
-    if (text === "") {
-      Vibration.vibrate(1000);
-      Alert.alert("Ops!", "Você deve digitar um filme");
-    } else {
-      Vibration.vibrate(1000);
-      Alert.alert("Você procurou por:", `${text}`);
+  /* Capturando e registrando em state
+  o filme que o usuário deseja pesquisar */
+  const filmeDigitado = (valorDigitado) => {
+    /* valorDigitado (nome pode ser qualquer um) 
+    é um parâmetro automático vindo do evento onChangeText */
+    setFilme(valorDigitado);
+  };
+
+  const buscarFilmes = () => {
+    /* Se o state filme não foi definido/indicado/preenchido */
+    if (!filme) {
+      Vibration.vibrate(500);
+      return Alert.alert("Ops!", "Você deve digitar um filme!");
     }
+
+    Alert.alert("Você procurou por:", filme);
   };
 
   return (
     <SafeContainer>
-      <View style={styles.subContainer}>
-        <Text>Star Trek? O Poderoso Chefão? A trilogia Senhor dos Aneis?</Text>
-        <Text>Localize um filme que você viu ou gostaria de ver!</Text>
-        <View style={styles.viewForm}>
+      <View style={estilos.subContainer}>
+        <Text style={estilos.texto}>
+          Star Trek? O Poderoso Chefão? A trilogia Senhor dos Anéis?
+        </Text>
+        <Text style={estilos.texto}>
+          Localize um filme que você viu ou gostaria de ver!
+        </Text>
+
+        <View style={estilos.viewForm}>
           <Ionicons name="film" size={44} color="#5451a6" />
           <TextInput
-            style={styles.input}
-            onChangeText={onChangeText}
-            value={text}
+            onChangeText={filmeDigitado}
+            onSubmitEditing={buscarFilmes}
+            style={estilos.campo}
             placeholder="Digite o filme"
             placeholderTextColor="#5451a6"
+            maxLength={30}
             autoFocus
             enterKeyHint="search"
           />
         </View>
-        <Button title="Pesquisar" color="#5451a6" onPress={pesquisar} />
+        <Button onPress={buscarFilmes} title="Procurar" color="#5451a6" />
       </View>
     </SafeContainer>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   subContainer: {
     flex: 1,
     padding: 16,
   },
-  input: {
-    borderWidth: 1,
-    padding: 8,
-    flex: 0.95,
-    color: "#5451a6",
-    borderColor: "#5451a6",
+  texto: {
+    marginVertical: 8,
   },
   viewForm: {
     marginVertical: 8,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  campo: {
+    flex: 0.95,
+    borderWidth: 1,
+    borderColor: "#5451a6",
+    padding: 8,
   },
 });
