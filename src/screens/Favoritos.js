58d0a0ee4ce032206f3be9493df3f5b30853c744
André Marques <andre.m.d.abreu@gmail.com>
2024-03-14 11:29:20 -0300

    Função para puxar os dados dos favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,7 +1,45 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import SafeContainer from "../components/SafeContainer";
 
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default function Favoritos() {
+  /* State para registrar os dados carregados do storage */
+  const [listaFavoritos, setListaFavoritos] = useState([]);
+
+  /* useEffect será disparado assim que o usuário entrar
+  na tela Favoritos (portanto, somente uma vez) */
+  useEffect(() => {
+    const carregarFavoritos = async () => {
+      try {
+        /* Acessamos o storage criado previamente e guardamos
+        as strings de dados. */
+        const dados = await AsyncStorage.getItem("@favoritoscaixadeletra");
+
+        /* Se houver dados, transformamos em objetos (JSON.parse)
+        e atualizamos o state com a lista de favoritos */
+        if (dados) {
+          setListaFavoritos(JSON.parse(dados));
+        }
+      } catch (error) {
+        console.error("Erro ao carregar os dados: " + error);
+        Alert.alert("Erro", "Erro ao carregar os dados.");
+      }
+    };
+    carregarFavoritos();
+  }, []);
+
+  // Log no state
+  console.log(listaFavoritos);
+
   return (
     <SafeContainer>
       <View style={styles.subContainer}>
