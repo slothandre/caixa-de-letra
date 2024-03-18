f7ee48ea6b605026859db7e54ba0492ea1aec47f
André Marques <andre.m.d.abreu@gmail.com>
2024-03-18 08:23:51 -0300

    Update Favoritos.js

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -38,8 +38,48 @@ export default function Favoritos() {
     carregarFavoritos();
   }, []);
 
-  // Log no state
-  console.log(listaFavoritos);
+  const excluirTodosFavoritos = async () => {
+    Alert.alert(
+      "Excluir TODOS?",
+      "Tem certeza que deseja excluir TODOS os favoritos",
+      [
+        /* Propriedade style (aparência do botão):
+        SOMENTE NO IOS que faz diferença */
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+        {
+          text: "Sim, manda ver",
+          style: "destructive",
+          onPress: async () => {
+            // Removemos nosso storage de favoritos
+            await AsyncStorage.removeItem("@favoritoscaixadeletra");
+
+            // Atualizamos o state para sejam removidos da tela
+            setListaFavoritos([]);
+          },
+        },
+      ]
+    );
+  };
+
+  const excluir = async (idFilme) => {
+    /* Gerar uma nova lista de favoritos EXCETO o filme
+    que será removido */
+    const novaListaDeFavoritos = listaFavoritos.filter(
+      (filmeDaLista) => filmeDaLista.id !== idFilme
+    );
+
+    /* Atualizar o state (memória) com os dados da nova lista SEM o filme removido */
+    setListaFavoritos(novaListaDeFavoritos);
+
+    /* Atualizar o storage (memória física) com os dados da nova lista SEM o filme removido */
+    await AsyncStorage.setItem(
+      "@favoritosdahora",
+      JSON.stringify(novaListaDeFavoritos)
+    );
+  };
 
   return (
     <SafeContainer>
@@ -47,21 +87,35 @@ export default function Favoritos() {
         <View style={styles.viewFavoritos}>
           <Text style={styles.texto}>Quantidade: {listaFavoritos.length}</Text>
 
-          <Pressable style={styles.botaoExcluirFavoritos}>
-            <Text style={styles.textoBotao}>
-              <Ionicons name="trash-outline" size={16} /> Excluir favoritos
-            </Text>
-          </Pressable>
+          {listaFavoritos.length > 0 && (
+            <Pressable
+              onPress={excluirTodosFavoritos}
+              style={styles.botaoExcluirFavoritos}
+            >
+              <Text style={styles.textoBotao}>
+                <Ionicons name="trash-outline" size={16} /> Excluir favoritos
+              </Text>
+            </Pressable>
+          )}
         </View>
+
         <ScrollView showsVerticalScrollIndicator={false}>
           {listaFavoritos.map((filme) => {
             return (
-              <View style={styles.item} key={filme.id}>
-                <Pressable style={styles.botaoFilme}>
+              <View key={filme.id} style={styles.item}>
+                <Pressable
+                  onPress={() => {
+                    navigation.navigate("Detalhes", { filme });
+                  }}
+                  style={styles.botaoFilme}
+                >
                   <Text style={styles.titulo}>{filme.title}</Text>
                 </Pressable>
-                <Pressable style={styles.botaoExcluir}>
-                  <Ionicons name="trash" size={16} />
+                <Pressable
+                  onPress={() => excluir(filme.id)}
+                  style={styles.botaoExcluir}
+                >
+                  <Ionicons color="white" name="trash" size={16} />
                 </Pressable>
               </View>
             );
@@ -84,16 +138,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 8,
   },
-  texto: {
-    marginVertical: 8,
-  },
+  texto: { marginVertical: 8 },
   botaoExcluirFavoritos: {
     borderWidth: 1,
     borderColor: "red",
     padding: 8,
     borderRadius: 4,
   },
-  textoBotao: {
-    color: "red",
+  textoBotao: { color: "red" },
+  item: {
+    padding: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#eee8fc",
+    marginBottom: 8,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  botaoFilme: { flex: 0.9 },
+  titulo: { fontSize: 14 },
+  botaoExcluir: {
+    backgroundColor: "darkred",
+    padding: 4,
+    borderRadius: 4,
   },
 });
